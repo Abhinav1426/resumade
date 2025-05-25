601a723f6d03d1da68ce4b8939352a5a71b1c255
Abhinav1426 <abhigba1426@gmail.com>
2025-05-25 08:23:27 +0530

    1234

--- src/main.py
+++ src/main.py
@@ -1,15 +1,8 @@
 from src.FileOperations import save_json_to_file, extract_text_from_pdf
 from src.llm_invoker import parse_pdf_to_json
 
-
-
-
-
-
-
 if __name__ == '__main__':
     pdfPath="C:\\Users\\Abhinav\\Downloads\\resume (6).pdf"
-
     pdfText = extract_text_from_pdf(pdfPath)
     structured_data = parse_pdf_to_json(pdfText)
     print(structured_data)
